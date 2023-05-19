eecb20e099be0f928cd92d6d151a8ef18c8cb3a9
WagnerRPaz <wagner.paz1109@gmail.com>
2023-05-19 14:09:47 -0300

    first commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+E-mail e senha para entrar na aplicação:
+
+e-mail: admin@admin.com
+senha: admin
---
+++ TrabalhoSite/src/main/java/com/mycompany/trabalhosite/historicoo.java
@@ -0,0 +1,25 @@
+package com.mycompany.trabalhosite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public class historicoo {
+
+    private List<String> historico;
+
+    public historicoo() {
+        historico = new ArrayList<>();
+    }
+
+    public void addPage(String page) {
+        historico.add(page);
+    }
+
+    public List<String> getHistoricoo() {
+        return historico;
+    }
+}
